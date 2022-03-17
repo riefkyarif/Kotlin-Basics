3a36b56162c5687f9f5b19fed284eaf4b031b06c
riefkyarif <riefkyar.ed@protonmail.com>
2022-03-17 13:15:12 +0700

    add hbd code kt

---
+++ basics.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Happy Birthday, Rover!")
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    println("")
+    println("You are already 5!")
+    println("5 is the very best age to celebrate!")
+}
